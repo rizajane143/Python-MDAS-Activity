aa304c9f38b3ad65df6631f728c9651f30f9870c
rizajane143 <rrdelacruz@paterostechnologicalcollege.edu.ph>
2026-03-11 17:57:01 +0800

    program.py for exercise 3

---
+++ program.py
@@ -0,0 +1,30 @@
+# My first simple script
+
+print("Hello, I'm Riza Jane Dela Cruz! BSIT 1-V")
+
+my_age = 19
+student_name = "Riza Jane"
+
+print(my_age)
+print(student_name)
+
+
+member_nicole = "Nicole"
+member_joan_rose = "Joan Rose"
+member_marjorie = "Marjorie"
+member_lourenz = "Lourenz"
+member_mico = "Mico"
+
+print(member_nicole)
+print(member_joan_rose)
+print(member_marjorie)
+print(member_lourenz)
+print(member_mico)
+
+Color1, Color2, Color3 = "Pink", "Blue", "Green"
+
+print(Color1, Color2, Color3)
+
+status1 = status2 = status3 = "Enrolled"
+
+print(status1, status2, status3)
